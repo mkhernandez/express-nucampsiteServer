1eb77b05d1bef63a15fff9afb796443ac2b73c57
Mark Hernandez <markkimhernandez@gmail.com>
2020-08-15 14:43:24 -0500

    Basic Authentication

--- app.js
+++ app.js
@@ -26,8 +26,42 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Using authentication before serving our static files. We are creating our own function
+//to handle authentication
+function auth(req, res, next) {
+  console.log(req.headers);//For development purposes only
+  const authHeader = req.headers.authorization;//Grab the auth header out of the request header
+  if(!authHeader) { //if no authentication information then user access is denied. Create an error message and return that message
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');////This lets the client know that the server is requesting authentication
+    err.status = 401;//Standard error code for no credentials provided
+    return next(err);//Handle error message by sending it back to express to handle and send a new request for credentials
+  }
+
+  //when an authorization header is provided the string is decoded and parsed into username:password format
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];//contains the username
+  const pass = auth[1];//contains the password
+  
+  //Basic check for correct username and password
+  if(user === 'admin' && pass === 'password') {
+    return next(); //user is authorized
+  }else {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+}
+
+app.use(auth);//incorportate our function to authenticate
+
+//Serve up our static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Define our endpoints to navigate to for our app. 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
