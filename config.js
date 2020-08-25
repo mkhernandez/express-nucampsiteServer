ea6345f940da92f31b38bd4420543a2c7d25f543
Mark Hernandez <markkimhernandez@gmail.com>
2020-08-24 23:41:53 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '711511982732722',
+        clientSecret: '707b5c5cb3d283b76c5920c3ccdb121c'
+    }
 }
